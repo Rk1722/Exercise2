44bf666c451799127099d71bce919106617efef4
Riyaaz Khan <riyaaz.khan22@imperial.ac.uk>
2023-10-19 23:43:36 +0100

    Changes1

--- tests/test_exercise_2_3.py
+++ tests/test_exercise_2_3.py
@@ -1,4 +1,7 @@
+from typing import Literal
 import pytest
+import os
+import sys
 
 
 def test_import_isprime():
@@ -11,7 +14,9 @@ def test_import_isprime():
                                           (4, False),
                                           (47, True),
                                           (100, False)])
-def test_isprime(value, prime):
+def test_isprime(value: Literal[1, 2, 3, 4, 47, 100], prime: bool):
     from math_utils.primes import isprime
 
     assert isprime(value) == prime, "Incorrect return value from isprime"
+
+
