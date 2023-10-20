ad90e27a92f2c9c180c2875ad8bfd1f483c7fe06
Riyaaz Khan <riyaaz.khan22@imperial.ac.uk>
2023-10-20 12:32:02 +0100

    Challenge5

--- tests/test_exercise_2_5.py
+++ tests/test_exercise_2_5.py
@@ -1,11 +1,14 @@
 
+from Exercise2.tests import isprime
+
+
 def test_import():
     '''Ensure we can import isprime in the new location.'''
-    from math_utils import isprime
+    from Exercise2.tests import isprime
 
 
 def test_import_correct_function():
-    from math_utils import isprime, primes
+    from math_utils import primes
 
     assert primes.isprime is isprime, \
         "math_utils.isprime is not the same function as math_utils.primes.isprime"
